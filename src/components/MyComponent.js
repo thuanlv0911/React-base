de12da177983e035bdb050e3c1e56308bca1da44
thuanlv0911 <thuanlv0911@gmail.com>
2025-06-01 09:41:45 +0700

    video 23

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,14 +2,17 @@
 // function component
 
 import React from "react";
-import UserInfor from "./UserInfro";
+import UserInfor from "./UserInfor";
+import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
   // JSX
   render() {
+    const myInfor = ["THUANLV", "hi"];
     return (
       <div>
         <UserInfor></UserInfor>
+        <DisplayInfor name="thuanlv" age={21} myInfor={myInfor}></DisplayInfor>
       </div>
     );
   }
