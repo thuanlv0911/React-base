14065315404befb5d62579d64dedcca0567a7bbd
thuanlv0911 <thuanlv0911@gmail.com>
2025-06-05 12:12:41 +0700

    video 25

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,23 +7,31 @@ import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
   // JSX
-  listUsers = [
-    {
-      id: 1,
-      name: "thuan",
-      age: 20,
-    },
-    {
-      id: 2,
-      name: "thuanlv",
-      age: 21,
-    },
-  ];
+  state = {
+    listUsers: [
+      {
+        id: 1,
+        name: "thuan",
+        age: "16",
+      },
+      {
+        id: 2,
+        name: "thuanlv",
+        age: "21",
+      },
+      {
+        id: 3,
+        name: "thuanlv09",
+        age: "20",
+      },
+    ],
+  };
   render() {
     return (
       <div>
         <UserInfor></UserInfor>
-        <DisplayInfor listUsers={this.listUsers}></DisplayInfor>
+        <hr />
+        <DisplayInfor listUsers={this.state.listUsers}></DisplayInfor>
       </div>
     );
   }
