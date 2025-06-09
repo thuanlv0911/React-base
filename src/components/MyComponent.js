c4f8a09491db817961a10e6a0ce07f459222d92f
thuanlv0911 <thuanlv0911@gmail.com>
2025-06-09 12:02:33 +0700

    video 37

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,65 +1,112 @@
 // class component
 // function component
 
-import React from "react";
+import React, { useState } from "react";
 import AddUserInfor from "./AddUserInfor";
 import DisplayInfor from "./DisplayInfor";
 
-class MyComponent extends React.Component {
-  // JSX
-  state = {
-    listUsers: [
-      {
-        id: 1,
-        name: "thuan",
-        age: "16",
-      },
-      {
-        id: 2,
-        name: "thuanlv",
-        age: "21",
-      },
-      {
-        id: 3,
-        name: "thuanlv09",
-        age: "20",
-      },
-    ],
+// class MyComponent extends React.Component {
+//   // JSX
+//   state = {
+//     listUsers: [
+//       {
+//         id: 1,
+//         name: "thuan",
+//         age: "16",
+//       },
+//       {
+//         id: 2,
+//         name: "thuanlv",
+//         age: "21",
+//       },
+//       {
+//         id: 3,
+//         name: "thuanlv09",
+//         age: "20",
+//       },
+//     ],
+//   };
+
+//   handleAddNewUser = (userObj) => {
+//     this.setState({
+//       listUsers: [userObj, ...this.state.listUsers],
+//     });
+//   };
+
+//   handleDeleteUser = (userID) => {
+//     let listUserClone = [...this.state.listUsers];
+//     listUserClone = listUserClone.filter((user) => {
+//       return userID !== user.id;
+//     });
+//     this.setState({
+//       listUsers: listUserClone,
+//     });
+//   };
+
+//   render() {
+//     const test = "Hello world with React";
+//     return (
+//       <React.Fragment>
+//         {test}
+//         <div className="a">
+//           <AddUserInfor handleAddNewUser={this.handleAddNewUser}></AddUserInfor>
+//           <hr />
+//           <DisplayInfor
+//             listUsers={this.state.listUsers}
+//             handleDeleteUser={this.handleDeleteUser}
+//           ></DisplayInfor>
+//         </div>
+//         <div className="b"></div>
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+const MyComponent = (props) => {
+  const [listUsers, setListUsers] = useState([
+    {
+      id: 1,
+      name: "thuan",
+      age: "16",
+    },
+    {
+      id: 2,
+      name: "thuanlv",
+      age: "21",
+    },
+    {
+      id: 3,
+      name: "thuanlv09",
+      age: "20",
+    },
+  ]);
+
+  const handleAddNewUser = (userObj) => {
+    setListUsers([userObj, ...listUsers]);
   };
 
-  handleAddNewUser = (userObj) => {
-    this.setState({
-      listUsers: [userObj, ...this.state.listUsers],
-    });
-  };
-
-  handleDeleteUser = (userID) => {
-    let listUserClone = [...this.state.listUsers];
+  const handleDeleteUser = (userID) => {
+    let listUserClone = listUsers;
     listUserClone = listUserClone.filter((user) => {
       return userID !== user.id;
     });
-    this.setState({
-      listUsers: listUserClone,
-    });
+    setListUsers(listUserClone);
   };
-
-  render() {
-    const test = "Hello world with React";
-    return (
-      <React.Fragment>
-        {test}
-        <div className="a">
-          <AddUserInfor handleAddNewUser={this.handleAddNewUser}></AddUserInfor>
-          <hr />
-          <DisplayInfor
-            listUsers={this.state.listUsers}
-            handleDeleteUser={this.handleDeleteUser}
-          ></DisplayInfor>
-        </div>
-        <div className="b"></div>
-      </React.Fragment>
-    );
-  }
-}
+  const test = "Hello world with React";
+  return (
+    <React.Fragment>
+      {test}
+      <div className="a">
+        <AddUserInfor handleAddNewUser={handleAddNewUser}></AddUserInfor>
+        <hr />
+        <DisplayInfor
+          listUsers={listUsers}
+          handleDeleteUser={handleDeleteUser}
+        ></DisplayInfor>
+      </div>
+      <div className="b"></div>
+    </React.Fragment>
+  );
+};
 
 export default MyComponent;
