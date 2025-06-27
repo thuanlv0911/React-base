a041a9cc643406da0f072f964fc11cae526579df
thuanlv0911 <thuanlv0911@gmail.com>
2025-06-27 18:05:42 +0700

    video 48

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
-// import logo from "./logo.svg";
-import { Button } from "bootstrap";
 import "./App.scss";
-// import { useDispatch, useSelector } from "react-redux";
-// import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-// import MyComponent from "./components/MyComponent";
-// import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./components/Header";
 
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <div className="app-container">
-//         {/* Hello world with Erik */}
-//         <MyComponent></MyComponent>
-//       </div>
-//     );
-//   }
-// }
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header/Header";
+import { Link } from "react-router-dom";
 
 const App = () => {
-  // const count = useSelector((state) => state.counter.count);
-  // const dispatch = useDispatch();
-
   return (
     <div className="app-container">
       <Header />
+      <div>
+        Test
+        <div>
+          <button>
+            <Link to="/user">Go to user page</Link>
+          </button>
+          <button>
+            <Link to="/admin">Go to admin page</Link>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,17 +2,24 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Users</Nav.Link>
-            <Nav.Link href="#link">Admin</Nav.Link>
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+            <Link to="/user" className="nav-link">
+              Users
+            </Link>
+            <Link to="admin" className="nav-link">
+              Admin
+            </Link>
           </Nav>
           <Nav>
             <NavDropdown title="Setting" id="basic-nav-dropdown">
